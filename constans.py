9bf9a316761bc42049728dacfbb1582775153fd4
Santiago Gómez <santi.gomezg.13@gmail.com>
2024-01-31 20:11:44 -0500

    Add a new constant

--- constans.py
+++ constans.py
@@ -1,2 +1,13 @@
+from datetime import datetime as dt
+
 HOSTS_PATH = 'C:\Windows\System32\drivers\etc\hosts'
 LOCALHOST = '127.0.0.1'
+LABOR_TIME_START = dt(dt.now().year, dt.now().month, dt.now().day, 5,50)
+LABOR_TIME_END = dt(dt.now().year, dt.now().month, dt.now().day, 13)
+WEBSITES = [
+    "www.youtube.com",
+    "www.facebook.com",
+    "www.instagram.com",
+    "www.netflix.com",
+    "www.spotify.com"
+]
